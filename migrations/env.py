a48d8b7e455cea0a66ba87e118480a8def5956ce
i-walk-away <orchidfake999@yandex.ru>
2026-01-19 00:47:25 +0300

    improve migration.sh script and handle edge cases

--- migrations/env.py
+++ migrations/env.py
@@ -12,8 +12,6 @@ sys.path.append(str(project_root))
 
 from cfg.cfg import settings
 from src.app.domain.models.db import Base
-from src.app.domain.models.db import lesson as _lesson
-from src.app.domain.models.db import lesson as _lesson
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
