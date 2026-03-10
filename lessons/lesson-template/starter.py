35077119f518629c5568e0417023652e78a62b09
i-walk-away <orchidfake999@yandex.ru>
2026-03-10 21:00:27 +0300

    add guidelines for contributors

--- lessons/lesson-template/starter.py
+++ lessons/lesson-template/starter.py
@@ -6,12 +6,12 @@ class UserProfile(BaseModel):
 
 
 # INFORMATION FOR CONTRIBUTORS
-#
+
 # this script is what will be displayed
 # to the user in the code editor by default in your lesson.
 # the user will then build upon your starter script
 # in order to complete the assignment
-#
+
 # it should give structure, but not implementation:
 # try not to define any fields, unless you think that
 # manually creating a BaseModel is unneccesary work
@@ -19,12 +19,16 @@ class UserProfile(BaseModel):
 # if your lesson is mostly focused on writing validators,
 # you can define a model in its entirety, so that the user
 # only has to write validators
-#
+
+# make sure that your test cases in `cases.yaml` use the exact names
+# of the classes / functions as they're defined here in the starter script.
+# (this UserProfile class can be fully accessed and instantiated within your test case scripts)
+
 # NOTE: always import everything that is needed to complete
 # the assignment. refrain from absolute imports
 # example: if your lesson is about validators,
 # import neccessary validators explicitly in your starter script
-#
+
 # don't worry if you think your starter script is bad or
 # if you think it does not meet the recommendations provided
 # in this commented block. i am happy to help making it better :)
